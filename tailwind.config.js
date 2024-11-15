8ec91ac2b3d453fcee3adb3c744f8cfe07b42514
Camilo Garavito <camigaravito03@gmail.com>
2024-11-15 15:54:03 -0500

    version 0.1b - Fuyu - Tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/views/**/*.pug",
+    "./public/js/**/*.js",
+    "./public/**/*.html",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
