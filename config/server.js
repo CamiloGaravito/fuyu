fa5e1e56539e48636049a4462f11e6a6f232d8a8
Camilo Garavito <camigaravito03@gmail.com>
2024-11-15 17:53:38 -0500

    version 0.1e - Fuyu - Server

---
+++ config/server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const logger = require("./logger");
+const sequelize = require("./database");
+const get_message = require("./message");
+const port = process.env.PORT;
+
+const app = express();
+
+const start_server = async () => {
+  try {
+    await sequelize.authenticate();
+
+    app.listen(port, () => {
+      logger.info(get_message("fay4", { port }));
+    });
+  } catch (error) {
+    logger.error(get_message("fay3"), error);
+    process.exit(1);
+  }
+};
+
+module.exports = start_server;
--- index.js
+++ index.js
@@ -1,24 +1,4 @@
 require("dotenv").config();
+const start_server = require("./config/server");
 
-const express = require("express");
-const port = process.env.PORT;
-const app = express();
-
-const get_message = require("./config/message");
-const sequelize = require("./config/database");
-const logger = require("./config/logger");
-
-const init_app = async () => {
-  try {
-    await sequelize.authenticate();
-
-    app.listen(port, () => {
-      logger.info(get_message("fay4", { port }));
-    });
-  } catch (error) {
-    logger.error(get_message("fay3"), error);
-    process.exit(1);
-  }
-};
-
-init_app();
+start_server();
