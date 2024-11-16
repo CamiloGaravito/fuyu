8c0a114eca7ea4163f98f275b6440c26ea1143da
Camilo Garavito <camigaravito03@gmail.com>
2024-11-16 13:31:22 -0500

    version 0.1fc - Fuyu - Migrations & Seeders

--- fuyu.js
+++ fuyu.js
@@ -2,17 +2,30 @@ const { execSync } = require("child_process");
 const logger = require("./config/logger");
 const get_message = require("./config/message");
 
-const args = process.argv.slice(2).join(" ");
-const sequelizeCommand = `npx sequelize-cli ${args}`;
+const buildCommand = (type, args) => {
+  switch (type) {
+    case "model":
+      return `npx sequelize-cli model:generate --name ${args[0]} --attributes ${args[1]}`;
+    case "migration":
+      return `npx sequelize-cli migration:generate --name ${args[0]}`;
+    case "seeder":
+      return `npx sequelize-cli seed:generate --name ${args[0]}`;
+    default:
+      throw new Error("Comando no reconocido.");
+  }
+};
+
+const [type, ...args] = process.argv.slice(2);
 
 try {
+  const sequelizeCommand = buildCommand(type, args);
   execSync(sequelizeCommand, { stdio: "inherit" });
 
-  if (sequelizeCommand.includes("model:generate")) {
+  if (type === "model") {
     logger.info(get_message("fay6", { sequelizeCommand }));
-  } else if (sequelizeCommand.includes("migration:generate")) {
+  } else if (type === "migration") {
     logger.info(get_message("fay7", { sequelizeCommand }));
-  } else if (sequelizeCommand.includes("seed:generate")) {
+  } else if (type === "seeder") {
     logger.info(get_message("fay8", { sequelizeCommand }));
   }
 } catch (error) {
